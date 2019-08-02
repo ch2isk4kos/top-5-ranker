861d601a7c38c93a645dfb6195658ce93e8069a8
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-02 00:16:20 -0400

    add new && create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
 end

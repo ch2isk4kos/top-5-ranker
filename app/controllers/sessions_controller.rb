9f107257bd1ed15f8f43eea9f0f9d418c5efd0e2
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-02 01:18:57 -0400

    add crud actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+    # create new session
+    def create
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You have successfully logged in"
+            redirect_to user
+        else
+            flash.now[:danger] = "There was a problem with your login information."
+            render :new
+        end
+    end
+
+    # logout
+    def destroy
+        session.clear
+        # flash[:success] = "You have successfully logged out" 
+        redirect_to root_path
+    end
 end

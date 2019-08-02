84b133b82ae2fa05d021c975066af9885aa71e40
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-02 01:58:35 -0400

    add login and logout requests

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
     get 'signup', to: 'users#new'
     post 'signup', to: 'users#create'
 
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+
+    delete 'logout', to: 'sessions#destroy'
+
     resources :users, except: [:index]
     resources :categories
     resources :rankings
@@ -15,8 +20,5 @@ Rails.application.routes.draw do
     # resources :sports, except: [:edit, :update, :destroy] do
     #     resources :rankings, only: [:new, :create, :show]
     # end
-
-
-
-
+    
 end

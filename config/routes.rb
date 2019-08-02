21ddf8e9b3c13e7ed627d176a09be6840ad52f4c
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-01 23:44:06 -0400

    add route to root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :teams
-  resources :sports
-  resources :players
-  resources :rankings
-  resources :users
-  resources :categories
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    root 'welcome#home'
+
+    resources :teams
+    resources :sports
+    resources :players
+    resources :rankings
+    resources :users
+    resources :categories
 end
